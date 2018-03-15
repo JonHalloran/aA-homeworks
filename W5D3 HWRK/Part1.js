0c654a18352bbe59408e883c6e97123879fdd230
JonHalloran <angmar139@hotmail.com>
2018-03-15 08:53:55 -0700

    W5D4

---
+++ W5D3 HWRK/Part1.js
@@ -0,0 +1,7 @@
+function madlibs(verb, adj, noun) {
+  return "We shall" + verb.toUpperCase() + "the" + adj.toUpperCase() + noun.toUpperCase()
+}
+
+function isSubstring(searchString, subString) {
+  searchString.include(subString)
+}
