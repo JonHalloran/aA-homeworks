834fab979e7f4f80f31540837cf49d19edf429a9
JonHalloran <angmar139@hotmail.com>
2018-03-23 08:56:12 -0700

    W6D4

--- W6D4 HWRK/script.js
+++ W6D4 HWRK/script.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
   // toggling restaurants
 
-  const toggleLi = (e) => {
+  const toggleLi = e => {
     const li = e.target;
     if (li.className === "visited") {
       li.className = "";
@@ -10,22 +10,29 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   };
 
-  document.querySelectorAll("#restaurants li").forEach((li) => {
+  const addPlace = e => {
+    e.preventDefault();
+    const form = e.target;
+    const textField = form.getElementsByClassName("favorite-input")[0];
+
+    const rests = document.getElementById("restaurants");
+    rests.appendChild(textField.value);
+    console.log(document);
+    textField.value = "";
+  };
+
+  document.querySelectorAll("#restaurants li").forEach(li => {
     li.addEventListener("click", toggleLi);
   });
-
-
+  document.querySelectorAll("#favorite-places").forEach(form => {
+    form.addEventListener("submit", addPlace);
+  });
 
   // adding SF places as list items
 
   // --- your code here!
 
-
-
   // adding new photos
 
   // --- your code here!
-
-
-
 });
